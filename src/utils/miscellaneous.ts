8063ab1976f31d32a62b1eafddf28c164325b1a0
Valentin Rubio <valentin.rubio99@gmail.com>
2023-01-30 10:43:41 +0100

    autoCopyright error

--- src/utils/miscellaneous.ts
+++ src/utils/miscellaneous.ts
@@ -7,9 +7,12 @@ export const autoCopyright = (
 ) => {
   const elements = document.querySelectorAll("[autoCopyright='true']");
 
-  console.log(elements, elements.length);
-  elements.forEach((element) => {
+  if (elements.length === 0) {
+    throw new Error("autoCopyright: no elements found.");
+  } else {
+    elements.forEach((element) => {
       const currentYear = new Date().getFullYear();
       element.innerHTML = String(currentYear);
-  });
+    });
+  }
 };
